7f6a2cf265c217349ae0d96c1dc2cd38e612d8ab
Mahbubur Rahman <mail@mahbub.me>
2018-12-20 10:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if( cardNumber !== 'undefined' ) {
+  if( cardNumber !== 'undefined' && total() > 0 ) {
     cart.length = 0;
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }

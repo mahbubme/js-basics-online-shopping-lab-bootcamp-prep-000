c06c4549b536ecdb26a76c3eca2388f4bdcd27ea
Mahbubur Rahman <mail@mahbub.me>
2018-12-20 10:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if( !empty( cardNumber ) ) {
-     
+    cart.length = 0;
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
   return `Sorry, we don't have a credit card on file for you.`;
 }

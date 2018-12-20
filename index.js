704ff8fefd2627a28444b7134b17b2dadff96515
Mahbubur Rahman <mail@mahbub.me>
2018-12-20 10:00:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if( !empty( cardNumber ) ) {
-    
+     
   }
   return `Sorry, we don't have a credit card on file for you.`;
 }
